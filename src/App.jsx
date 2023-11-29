29449ea37e25b8474970d6fcaca315002fdce3ee
Jenna <jenna@brilltek.com.tw>
2023-11-30 00:14:16 +0800

    feat: unoCSS init & modify UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
 import React from "react"
 import Header from './components/Header'
-import useSize from "./hooks/useSize"
 import "./App.css"
 
 function App() {
-  const [width, height] = useSize()
-
   return (
-    <div className="h-screen bg-[#e3e3e3]">
+    <div className="h-screen">
       <Header />
-      <main className="mt-[60px] md:mt-[100px]">
-        <section className="flex justify-center pt-5 px-5 sm:pt-5 md:pt-11">
+      <main className="mt-[60px] bg-[#e3e3e3] lg:mt-[100px]">
+        {/* Banner */}
+        <section className="flex justify-center px-5 pt-10 lg:pt-14">
           <div className="w-full max-w-[1240px]">
-            <div className="relative w-full h-[410px] md:h-[650px]">
-              <div className="flex justify-center sm:justify-start mb-4">
-                <div className="w-[300px] h-auto sm:w-[440px] sm:h-[30px]">
-                  <img src="/2023-f2e.svg" className="" />
+            <div className="relative w-full h-[410px] md:h-[440px] lg:h-[650px]">
+              <div className="flex justify-center mb-4 sm:justify-start">
+                <div className="h-auto">
+                  <img src="/2023-f2e.svg" className="w-[20rem] md:w-[25rem] lg:w-[30rem]" />
                 </div>
               </div>
-              <div className="absolute md:top-1/2 md:-translate-y-1/2 flex flex-col gap-4">
-                <div className="w-[342px] h-[119px] md:w-[700px] md:h-[300px]">
-                  <img src={width < 768 ? "/slogan-m.svg" : "/slogan.svg"} />
+              <div className="absolute flex flex-col gap-4 lg:(top-1/2 -translate-y-1/2)">
+                <div className="w-[342px] h-[119px] md:(w-[400px] h-[190px]) lg:(w-[700px] h-[300px])">
+                  <img src="/slogan-m.svg" className="block md:hidden w-full h-full" />
+                  <img src="/slogan.svg" className="hidden md:block w-full h-full" />
                 </div>
                 <div className="flex">
                   <div className="w-[13px] h-[200px] pt-4">
                     <img src="/arrow.svg" className="w-full h-full" />
                   </div>
-                  <div className="w-[234px] h-[125px] md:w-[1000px] md:h-[123px] pl-5 mt-6">
-                    <img src={width < 768 ? "/name-en-m.svg" : "/name-en.svg"} className="w-full h-full" />
+                  <div className="pl-5 mt-10 md:(w-[650px] h-[80px]) lg:(w-[1000px] h-[123px])">
+                    <img src="/name-en-m.svg" className="block md:hidden w-full h-full" />
+                    <img src="/name-en.svg" className="hidden md:block w-full h-full" />
                   </div>
                 </div>
               </div>
-              <div className="absolute top-1/2 -translate-y-[110px] sm:-translate-y-[185px] md:-translate-y-[326px] lg:-translate-y-1/2 flex right-8 md:right-20">
-                <div className="relative z-10 w-[242px] h-[315px] sm:w-[300px] sm:h-[390px] md:w-[500px] md:h-[652px]">
+              <div className="absolute top-1/2 -translate-y-[110px] flex right-8 sm:-translate-y-[185px] md:-translate-y-[255px] lg:(-translate-y-[326px] right-20)">
+                <div className="relative z-10 w-[242px] h-[315px] sm:(w-[300px] h-[390px]) md:(w-[380px] h-[480px]) lg:(w-[500px] h-[652px])">
                   <img src="/candidate.webp" className="w-full h-full" />
                 </div>
                 <div className="w-[130px] h-[430px] absolute right-0 hidden lg:flex">
@@ -46,14 +46,12 @@ function App() {
           <div className="w-full max-w-[1240px]">
             <div className="flex gap-4 md:gap-[64px]">
               <img src="/slogan-en.svg" className="w-[19px] h-auto" />
-              <div className="w-auto p-4 bg-white border-4 border-[#4527a0] rounded-tl-[80px] rounded-br-[80px] shadow-[-5px_5px_0_0_rgba(69,39,160,1)] md:shadow-[-15px_20px_0_0_rgba(69,39,160,1)]">
-                <p className="text-xl md:text-[44px] font-bold text-center whitespace-pre-line mb-2.5">護航台灣幸福經濟 從照顧每一隻貓咪開始</p>
-                <p className="text-[15px] md:heading-4 whitespace-pre-line">
-                  {`我堅信，藉由推動更完善的貓咪福利和相關政策，更是間接地投資於台灣的未來。畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。透過完善的貓咪福利政策，為台灣的GDP 經濟帶來巨大效益。
-
-                  因此，我期望能在立法院內推進這些政策，確保每一隻貓咪都能得到他們應有的照顧，同時也為台灣的經濟發展助一臂之力。
-                  `}
+              <div className="flex flex-col gap-4 p-8 ml-4 bg-white border-solid border-5 border-[#4527a0] rounded-tl-[8rem] rounded-br-[8rem] drop-shadow-[-15px_20px_#41289a] md:(gap-8 ml-0)">
+                <p className="text-2xl md:text-3xl lg:text-4xl font-bold text-center whitespace-pre-line line-height-tight mb-2.5 break-keep">
+                  護航台灣幸福經濟 從照顧每一隻貓咪開始
                 </p>
+                <h4 className="text-lg md:text-xl lg:text-2xl">我堅信，藉由推動更完善的貓咪福利和相關政策，更是間接地投資於台灣的未來。畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。透過完善的貓咪福利政策，為台灣的GDP 經濟帶來巨大效益。</h4>
+                <h4 className="text-lg md:text-xl lg:text-2xl">因此，我期望能在立法院內推進這些政策，確保每一隻貓咪都能得到他們應有的照顧，同時也為台灣的經濟發展助一臂之力。</h4>
               </div>
             </div>
           </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,23 +18,23 @@ const MENU = [
 
 const Header = () => {
   return (
-    <header className="fixed w-full flex-center z-50 top-0 bg-white py-2 md:py-4 h-[60px] md:h-[100px]">
-      <div className="flex px-5 mx-auto justify-between items-center max-w-[1240px]">
-        <div className="flex items-center w-[150px] md:w-[200px]">
+    <header className="fixed w-full z-50 top-0 py-2 bg-white lg:(py-4 h-[100px]) lg:h-[60px]">
+      <div className="flex h-full px-5 mx-auto justify-between items-center max-w-[1240px]">
+        <div className="flex items-center w-[150px] md:w-[180px] lg:w-[200px]">
           <img src="/logo.png" alt="logo" className="w-full h-auto" />
         </div>
         <nav className="hidden md:block">
-          <ul className="flex items-center text-lg gap-x-4 lg:gap-x-10 font-bold">
+          <ul className="flex items-center md:text-md lg:(text-lg gap-x-10) gap-x-4 font-bold">
             {MENU.map((item) => (
               <li>
-                <a href={item.link} className="transition ease-in-out	hover:border-b-4 hover:pb-2 hover:border-[#41289a]">
+                <a href={item.link} className="transition ease-in-out	hover:(border-b-4 border-solid pb-2 border-[#41289a])">
                   {item.name}
                 </a>
               </li>
             ))}
-            <li className="bg-[#41289a] hover:bg-[#543DA4] rounded-full py-4 px-6 shake-animation">
+            <li className="bg-[#41289a] hover:bg-[#543DA4] rounded-full md:(py-2 px-4) lg:(py-4 px-6) shake-animation">
               <a href="#">
-                <button className="flex items-center gap-x-2 text-white text-xl font-bold">
+                <button className="flex items-center gap-x-2 text-white md:text-md lg:text-xl font-bold">
                   <img src="/coin.svg" className="w-7 h-7" />
                   小額捐款
                 </button>
@@ -42,6 +42,29 @@ const Header = () => {
             </li>
           </ul>
         </nav>
+        <div className="md:hidden">
+          <input type="checkbox" id="menu-toggle" class="m-menu-icon-input" />
+          <label for="menu-toggle" class="cursor-pointer">
+            <img src="menu.svg" class="w-7 h-7" color="#41289a" />
+          </label>
+          <div className="m-menu absolute transition ease-in-out bg-[#41289a] w-1/2 h-screen right-0 top-full">
+            <ul className="flex flex-col gap-8 text-right p-6 text-xl">
+              {MENU.map((item) => (
+                <li key={item.name}>
+                  <a href={item.link} className="text-white transition ease-in-out hover:(border-b-4 border-solid pb-2 border-white)">
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+              <li className="flex justify-end">
+                <a href="#" className="flex items-center gap-1 text-white transition ease-in-out hover:(border-b-4 border-solid pb-2 border-white)">
+                  <img src="/coin.svg" className="w-7 h-7" />
+                  小額捐款
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </header>
   )
