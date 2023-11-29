15607783078f26bc90df0e0ca13fe82640c5542f
CarrotMelody <aquamay0216@gmail.com>
2023-11-29 21:12:39 +0800

    feat: banner UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,80 @@
 import React from "react"
 import Header from './components/Header'
+import useSize from "./hooks/useSize"
 import "./App.css"
 
-const MENU = [
-  {
-    name: '候選人主張',
-    link: '#',
-  }, {
-    name: '最新活動',
-    link: '#',
-  }, {
-    name: '政策議題',
-    link: '#',
-  }, {
-    name: '民眾服務信箱',
-    link: '#',
-  }
-]
-
 function App() {
+  const [width, height] = useSize()
+
   return (
-    <div className="App">
-      <nav className="menu">
-        <div className="flex flex-row justify-between items-center">
-          <div className="flex items-center w-[150px] h-[38px] md:w-[235px] md:h-[50px]">
-            <img src="/logo.png" alt="logo" className="w-40" />
+    <div className="h-screen bg-[#e3e3e3]">
+      <Header />
+      <main className="mt-[60px] md:mt-[100px]">
+        <section className="flex justify-center pt-5 px-5 sm:pt-5 md:pt-11">
+          <div className="w-full max-w-[1240px]">
+            <div className="relative w-full h-[410px] md:h-[650px]">
+              <div className="flex justify-center sm:justify-start mb-4">
+                <div className="w-[300px] h-auto sm:w-[440px] sm:h-[30px]">
+                  <img src="/2023-f2e.svg" className="" />
+                </div>
+              </div>
+              <div className="absolute md:top-1/2 md:-translate-y-1/2 flex flex-col gap-4">
+                <div className="w-[342px] h-[119px] md:w-[700px] md:h-[300px]">
+                  <img src={width < 768 ? "/slogan-m.svg" : "/slogan.svg"} />
+                </div>
+                <div className="flex">
+                  <div className="w-[13px] h-[200px] pt-4">
+                    <img src="/arrow.svg" className="w-full h-full" />
+                  </div>
+                  <div className="w-[234px] h-[125px] md:w-[1000px] md:h-[123px] pl-5 mt-6">
+                    <img src={width < 768 ? "/name-en-m.svg" : "/name-en.svg"} className="w-full h-full" />
+                  </div>
+                </div>
+              </div>
+              <div className="absolute top-1/2 -translate-y-[110px] sm:-translate-y-[185px] md:-translate-y-[326px] lg:-translate-y-1/2 flex right-8 md:right-20">
+                <div className="relative z-10 w-[242px] h-[315px] sm:w-[300px] sm:h-[390px] md:w-[500px] md:h-[652px]">
+                  <img src="/candidate.webp" className="w-full h-full" />
+                </div>
+                <div className="w-[130px] h-[430px] absolute right-0 hidden lg:flex">
+                  <img src="/name.svg" className="w-full h-full" />
+                </div>
+              </div>
+            </div>
           </div>
-          <ul className="flex items-center text-lg gap-[3vw] font-bold">
-            {MENU.map((item) => (
-              <li>
-                <a href={item.link} className="transition ease-in-out	hover:border-b-4 hover:pb-3 hover:border-[#41289a]">
-                  {item.name}
-                </a>
-              </li>
-            ))}
-            <li>
-              <a href="#">
-                <button className="flex flex-row items-center bg-[#41289a] text-white text-xl font-bold rounded-full py-4 px-6 shake-animation">
-                  <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path><path d="M14.8 9a2 2 0 0 0 -1.8 -1h-2a2 2 0 1 0 0 4h2a2 2 0 1 1 0 4h-2a2 2 0 0 1 -1.8 -1"></path><path d="M12 7v10"></path></svg>
-                  小額捐款
-                </button>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <section className="bg-[#e3e3e3] h-screen">
+        </section>
+        <section className="flex justify-center px-5">
+          <div className="w-full max-w-[1240px]">
+            <div className="flex gap-4 md:gap-[64px]">
+              <img src="/slogan-en.svg" className="w-[19px] h-auto" />
+              <div className="w-auto p-4 bg-white border-4 border-[#4527a0] rounded-tl-[80px] rounded-br-[80px] shadow-[-5px_5px_0_0_rgba(69,39,160,1)] md:shadow-[-15px_20px_0_0_rgba(69,39,160,1)]">
+                <p className="text-xl md:text-[44px] font-bold text-center whitespace-pre-line mb-2.5">護航台灣幸福經濟 從照顧每一隻貓咪開始</p>
+                <p className="text-[15px] md:heading-4 whitespace-pre-line">
+                  {`我堅信，藉由推動更完善的貓咪福利和相關政策，更是間接地投資於台灣的未來。畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。透過完善的貓咪福利政策，為台灣的GDP 經濟帶來巨大效益。
 
-      </section>
-      {/* 最新活動 */}
-      <section>
+                  因此，我期望能在立法院內推進這些政策，確保每一隻貓咪都能得到他們應有的照顧，同時也為台灣的經濟發展助一臂之力。
+                  `}
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* 最新活動 */}
+        <section>
 
-      </section>
-      {/* 政策議題 */}
-      <section>
+        </section>
+        {/* 政策議題 */}
+        <section>
 
-      </section>
-      {/* 小額捐款 */}
-      <section>
+        </section>
+        {/* 小額捐款 */}
+        <section>
 
-      </section>
-      {/* 民眾服務信箱 */}
-      <section>
+        </section>
+        {/* 民眾服務信箱 */}
+        <section>
 
-      </section>
+        </section>
+      </main>
       <footer>
 
       </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,48 @@
 import React from "react"
 
-const MENU_LIST = [
+const MENU = [
   {
-    key: 'zz',
-    label: '候選人主張'
+    name: '候選人主張',
+    link: '#',
   }, {
-    key: 'news',
-    label: '最新活動'
+    name: '最新活動',
+    link: '#',
   }, {
-    key: 'zcyt',
-    label: '政策議題'
+    name: '政策議題',
+    link: '#',
   }, {
-    key: 'email',
-    label: '民眾服務信箱'
-  }, {
-    key: 'money',
-    label: '小額捐款',
-    icon: <svg
-      fill="currentColor"
-      viewBox="0 0 16 16"
-      height="1.5rem"
-      width="1.5rem"
-    >
-      <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z" />
-    </svg>
+    name: '民眾服務信箱',
+    link: '#',
   }
 ]
 
 const Header = () => {
   return (
-    <header className="header">
-      <ul className="flex flex-row w-full gap-6 items-center">
-        {MENU_LIST.map((item) => (
-          <li key={item.key} className="flex flex-row items-center gap-2 last:ml-auto last:border last:rounded-full last:px-6 last:py-2">
-            {item.icon}
-            {item.label}
-          </li>
-        ))}
-      </ul>
+    <header className="fixed w-full flex-center z-50 top-0 bg-white py-2 md:py-4 h-[60px] md:h-[100px]">
+      <div className="flex px-5 mx-auto justify-between items-center max-w-[1240px]">
+        <div className="flex items-center w-[150px] md:w-[200px]">
+          <img src="/logo.png" alt="logo" className="w-full h-auto" />
+        </div>
+        <nav className="hidden md:block">
+          <ul className="flex items-center text-lg gap-x-4 lg:gap-x-10 font-bold">
+            {MENU.map((item) => (
+              <li>
+                <a href={item.link} className="transition ease-in-out	hover:border-b-4 hover:pb-2 hover:border-[#41289a]">
+                  {item.name}
+                </a>
+              </li>
+            ))}
+            <li className="bg-[#41289a] hover:bg-[#543DA4] rounded-full py-4 px-6 shake-animation">
+              <a href="#">
+                <button className="flex items-center gap-x-2 text-white text-xl font-bold">
+                  <img src="/coin.svg" className="w-7 h-7" />
+                  小額捐款
+                </button>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
